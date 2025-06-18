13d1d445f60dbb4f25c2acd70ae07c8cc58ea753
manviswa <viswamandem@gmail.com>
2025-06-18 09:46:53 +0530

    Second Variable script#

---
+++ 2.variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hello world, im learning scripting"
+PERSON1=$1 #This is the first value from command line
+PERSON2=$2 # This is the second value from command line
+
+echo "$PERSON1: HI $PERSON2, Good morning" 
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: im good, How are you $PERSON1"
