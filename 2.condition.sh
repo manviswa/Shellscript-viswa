2a2788df21322cd09c1572280db401b7e4dd85e8
manviswa <viswamandem@gmail.com>
2025-06-18 19:31:19 +0530

    installing mysql through condition script#

---
+++ 2.condition.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#installing sql database 
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]
+then
+  echo "ERROR:: Please run with root user access"
+  exit 1
+fi 
+# it is our responsibilty to check installation is success or not
+
+yum install sqlll -y 
+
+if [$? -ne 0]
+then 
+  echo "Installation of mysql is error"
+  exit
+else 
+  echo "Installation of mysql is success"
+fi
