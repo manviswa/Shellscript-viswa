117e2c94adab3a5d9dfcf3c751f95432d44ee403
manviswa <viswamandem@gmail.com>
2025-06-18 18:23:38 +0530

    Fifth Variable script#

--- 5.variable.sh
+++ 5.variable.sh
@@ -2,9 +2,9 @@
 
 echo "This is called array, instead of single value it can hold multiple values"
 
-PERSONS=("viswa","hema","jathin") #inside array we always refer values with index and it starts with 0
+PERSONS=("viswa" "hema" "jathin") #inside array we always refer values with index and it starts with 0
 
 echo "First person: ${PERSONS[0]}"
 
-echo "all persons: ${PERSONS[@]}"
+#echo "all persons: ${PERSONS[@]}"
 
