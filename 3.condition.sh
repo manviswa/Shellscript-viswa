bbf58f6474f8c7cd16a6fb0dbffe988ff4afd2fa
manviswa <viswamandem@gmail.com>
2025-06-19 19:55:10 +0530

    adding third condition script

--- 3.condition.sh
+++ 3.condition.sh
@@ -7,8 +7,7 @@ cd /opt/viswa
 if [ $? -ne 0 ]
 then 
   echo "Installation of mysql is error"
-  exit
 else 
   mkdir /opt/viswa
-  echo "Installation of mysql is success"
+  echo "Directory has been created"
 fi
