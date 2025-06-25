c18f737ff5f8a15306553d4f81da12800c541535
manviswa <viswamandem@gmail.com>
2025-06-25 12:02:32 +0530

    modified second loop script

--- 2.loop.sh
+++ 2.loop.sh
@@ -33,7 +33,7 @@ for i in $@
 
 do 
   yum list installed $i &>>$LOGFILE
-  if [ $? -ne 0];
+  if [ $? -ne 0]
   then 
       echo "$i is not installed, lets install it"
       yum install $i -y &>>$LOGFILE
